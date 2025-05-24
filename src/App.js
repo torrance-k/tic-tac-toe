6d5b933fb143cb6461d12b541eb2143f631825e6
torrance-k <knapptori6@gmail.com>
2025-05-24 14:10:29 -0400

    feat: use loops for board

--- src/App.js
+++ src/App.js
@@ -29,25 +29,27 @@ function Board({ xIsNext, squares, onPlay }) {
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
-  
+
+  const boardRows = [0, 1, 2];
+  const boardCols = [0, 1, 2];
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+        {boardRows.map((row) => (
+          <div key={row} className="board-row">
+            {boardCols.map((col) => {
+              const index = row * 3 + col;
+              return (
+                <Square
+                  key={index}
+                  value={squares[index]}
+                  onSquareClick={() => handleClick(index)}
+                />
+              );
+            })}
+          </div>
+        ))}
     </>
   );
 }
